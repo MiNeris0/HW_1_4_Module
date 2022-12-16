568ec8b28b97d62c692b080aaabbde70fdaf7720
MiNeris0 <seikolor@gmail.com>
2022-12-16 18:23:59 +0200

    Add project file

---
+++ HW_1_4_Module_work/HW_1_4_Module_work/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_1_4_Module_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
